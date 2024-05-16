1bf7f0d0b7bf20297b8f78bf95d2e87e1666270a
José Carlos Pereira <jcpereiradasilvafilho@gmail.com>
2024-05-16 14:58:22 -0300

    v.3 funcionando - valor_liquido automático

--- src/api/dividendo/controllers/dividendo.js
+++ src/api/dividendo/controllers/dividendo.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * dividendo controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::dividendo.dividendo');
+module.exports = createCoreController(
+  "api::dividendo.dividendo",
+  ({ strapi }) => ({
+    async create(ctx) {
+      
+      const resposta = await super.create(ctx);
+
+      if (resposta.data && resposta.data.attributes) {
+        const { valor_bruto, desconto } = resposta.data.attributes;
+
+        const valor_liquido = parseFloat(valor_bruto * ((100 - desconto) / 100)).toFixed(2);
+
+        await strapi.db.query("api::dividendo.dividendo").update({
+          where: { id: resposta.data.id },
+          data: {
+            valor_liquido: valor_liquido,
+          },
+        });
+
+        resposta.data.attributes.valor_liquido = valor_liquido;
+      }
+      return resposta;
+    },
+  })
+);
--- src/api/hospital/controllers/hospital.js
+++ src/api/hospital/controllers/hospital.js
@@ -1,37 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * hospital controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::hospital.hospital', ({ strapi }) => ({
-  async create(ctx) {
-    
-    const response = await super.create(ctx);
+module.exports = createCoreController(
+  "api::hospital.hospital",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const resposta = await super.create(ctx);
 
-    
-    if (response.data && ctx.request.body.data.especialidades) {
-      const hospitalId = response.data.id;
-      const especialidades = ctx.request.body.data.especialidades;
+      if (resposta.data && ctx.request.body.data.especialidades) {
+        const hospital_id = resposta.data.id;
+        const especialidades = ctx.request.body.data.especialidades;
 
-      
-      await Promise.all(especialidades.map(async (especialidadeId) => {
-        const especialidade = await strapi.db.query('api:especialidade.especialidade').findOne({ where: { id: especialidadeId}});
-        if (especialidade) {
-          const subdivisionName = `${response.data.attributes.nome} / ${especialidade.nome}`;
-          await strapi.db.query('api::lotacao.lotacao').create({
-            data: {
-              nome: subdivisionName,
-              hospital: hospitalId,
-              especialidade: especialidadeId
+        await Promise.all(
+          especialidades.map(async (especialidade_id) => {
+            const especialidade = await strapi.db
+              .query("api:especialidade.especialidade")
+              .findOne({ where: { id: especialidade_id } });
+            if (especialidade) {
+              const subdivisao_nome = `${resposta.data.attributes.nome} / ${especialidade.nome}`;
+              await strapi.db.query("api::lotacao.lotacao").create({
+                data: {
+                  nome: subdivisao_nome,
+                  hospital: hospital_id,
+                  especialidade: especialidade_id,
+                },
+              });
             }
-          });
-        }
-      }));
-    }
-
-    return response;
-  }
-}));
+          })
+        );
+      }
+      return resposta;
+    },
+  })
+);
